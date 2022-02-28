d3b84a65c521760bee0ae9593015f947af78b4c3
orozcoding <orozcoding@gmail.com>
2022-02-28 08:54:50 -0400

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="header">
+    <img src="../img/logo.png" alt="planet logo" />
+    <nav className="navBar">
+      <ul>
+        <li>Rockets</li>
+        <li>Missions</li>
+        <li>My Profile</li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
