985f3f692fb55574669db2a368d2c10cb25b327f
Billions Joel <joelatugonza@gmail.com>
2022-03-01 13:55:44 +0300

    Define ENDPOINT url

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,4 +1,6 @@
 const ADD_ROCKETS = 'redux/rockets/ADD_ROCKETS';
+const ENDPOINT = 'https://api.spacexdata.com/v3/rockets';
+
 export const rocketsReducer = () => null;
 
 export default rocketsReducer;
