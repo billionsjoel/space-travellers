6136b1093110fbb3c8366b90e24b47deef837140
Billions Joel <joelatugonza@gmail.com>
2022-03-01 16:41:33 +0300

    Fix linter issue

--- src/components/pages/rocket.js
+++ src/components/pages/rocket.js
@@ -3,15 +3,15 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getApiData } from '../../redux/rockets/rockets';
 
 function rocket() {
-	const rocketList = useSelector(({ rocketsReducer }) => rocketsReducer);
-	const dispatch = useDispatch();
-	useEffect(() => {
-		if (!rocketList.length) {
-			dispatch(getApiData());
-		}
-	}, []);
+  const rocketList = useSelector(({ rocketsReducer }) => rocketsReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!rocketList.length) {
+      dispatch(getApiData());
+    }
+  }, []);
 
-	return <div>Check Console for data</div>;
+  return <div>Check Console for data</div>;
 }
 
 export default rocket;
