da8657f040c3ec8247d6ef6a945efb6d7cf56e4d
Billions Joel <joelatugonza@gmail.com>
2022-03-01 09:47:32 +0300

    Add nomatch style page

--- src/components/pages/nomatch.js
+++ src/components/pages/nomatch.js
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 function nomatch() {
   return (
-    <div>
+    <div className="text-center pt-1">
       <h3>
         404 . Page does not exist. Go back to
         {' '}
