ffc3f521e8a74bcf70a24a2ff7a69ae16a01025f
Billions Joel <joelatugonza@gmail.com>
2022-03-01 22:48:38 +0300

    Refactor to fetch rockets

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -3,17 +3,16 @@ const ENDPOINT = 'https://api.spacexdata.com/v3/rockets';
 
 export const addRocket = (payload) => ({ type: ADD_ROCKETS, payload });
 
-export const getApiData = () => (dispatch) => fetch(ENDPOINT)
-  .then((response) => response.json())
-  .then((data) => {
-    Object.keys(data).forEach((rocket) => {
-      dispatch({
-        type: ADD_ROCKETS,
-        payload: { ...data[rocket] },
+export const getApiData = () => (dispatch) => {
+  const rockets = [];
+  fetch(ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      Object.keys(data).forEach((rocket) => {
+        rockets.push(data[rocket]);
       });
     });
-  });
-
+};
 const rocketsReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_ROCKETS:
