b6ae08c59d00e5803687dc6eb603ec6b79dea8a1
orozcoding <orozcoding@gmail.com>
2022-03-01 04:56:16 -0400

    Code missionsTable.js component

---
+++ src/components/missions/missionsTable.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './css/missions.scss';
+
+const MissionsTable = (props) => {
+  const { missionsList } = props;
+
+  return (
+    <table className="missionsTable d-flex">
+      <tbody>
+        <tr className="tableTitles">
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th aria-label="button join/leave" />
+        </tr>
+        {missionsList.map((mission) => (
+          <tr key={mission.id}>
+            <th>{mission.name}</th>
+            <th>{mission.description}</th>
+            {!mission.joined && (<th><p>Not a member</p></th>)}
+            {!mission.joined && (<th><button type="button">Join Mission</button></th>)}
+            {mission.joined && (<th><p>Active Member</p></th>)}
+            {!mission.joined && (<th><button type="button">Leave Mission</button></th>)}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+MissionsTable.propTypes = {
+  missionsList: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    joined: PropTypes.bool.isRequired,
+  })).isRequired,
+};
+
+export default MissionsTable;
