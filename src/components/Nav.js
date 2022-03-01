6aa22cdb89498dede2747a019565195b2e84582d
Billions Joel <joelatugonza@gmail.com>
2022-03-01 07:37:37 +0300

    Create Navlink component and export it

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,4 +2,24 @@ import './css/header.scss';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+function navLinks() {
+  return (
+    <div className="nav-container">
+      <nav className="navBar">
+        <ul className="d-flex">
+          <li className="click">
+            <Link to="/">Rockets</Link>
+          </li>
+          <li className="click">
+            <Link to="/missions">Missions</Link>
+          </li>
+          <li className="click">
+            <Link to="/my-profile">My Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
 
+export default navLinks;
