2078bf420ad9991ac110de58a36118d5f7efd4ba
Billions Joel <joelatugonza@gmail.com>
2022-03-01 12:38:36 +0300

    Use refactored class

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import NavLink from './Nav';
 
 const Header = () => (
-  <header className="header d-flex ai-c">
+  <header className="header d-flex align-items-center">
     <img src="./logo.png" alt="planet logo" className="click" />
     <NavLink />
   </header>
