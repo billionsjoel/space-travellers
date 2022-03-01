a95c68fba048b7f574e44b498c0d448ce029e59b
Billions Joel <joelatugonza@gmail.com>
2022-03-01 13:59:09 +0300

    Define addRocket function

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,6 +1,8 @@
 const ADD_ROCKETS = 'redux/rockets/ADD_ROCKETS';
 const ENDPOINT = 'https://api.spacexdata.com/v3/rockets';
 
+export const addRocket = (payload) => ({ type: ADD_ROCKETS, payload });
+
 export const rocketsReducer = () => null;
 
 export default rocketsReducer;
