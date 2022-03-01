30f325bcfc3981e627da9edfb0f5976e59012cb2
Billions Joel <joelatugonza@gmail.com>
2022-03-01 17:47:09 +0300

    Add Fix image display

--- src/components/pages/rockets/rocketItem.js
+++ src/components/pages/rockets/rocketItem.js
@@ -1,16 +1,16 @@
 function rocketItem({ company, description, image }) {
-	return (
-		<div className="rocket-container">
-			<div className="image">
-				<img src="{image}" alt="" />
-			</div>
-			<div className="rocket-details">
-				<h4>{company}</h4>
-				<p>{description}</p>
-				<button type="button">Reserve</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="rocket-container">
+      <div className="image">
+        <img src={image} alt="" />
+      </div>
+      <div className="rocket-details">
+        <h4>{company}</h4>
+        <p>{description}</p>
+        <button type="button">Reserve</button>
+      </div>
+    </div>
+  );
 }
 
 export default rocketItem;
