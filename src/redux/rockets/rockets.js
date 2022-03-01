e5caa8e24b97910bd66b574135f495d0690be0f6
Billions Joel <joelatugonza@gmail.com>
2022-03-01 16:28:21 +0300

    Return rockets

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -3,22 +3,25 @@ const ENDPOINT = 'https://api.spacexdata.com/v3/rockets';
 
 export const addRocket = (payload) => ({ type: ADD_ROCKETS, payload });
 
+const handleRockets = (rocket) => {
+  const rockets = [];
+  rockets.push(rocket);
+
+  return rockets;
+};
+
 export const getApiData = () => (dispatch) => fetch(ENDPOINT)
   .then((response) => response.json())
   .then((data) => {
     Object.keys(data).forEach((rocket) => {
       dispatch({
         type: ADD_ROCKETS,
-        payload: {
-          item_id: rocket, ...data[rocket][0],
-        },
+        payload: handleRockets(rocket),
       });
     });
   });
 
-const initialState = [];
-
-const rocketsReducer = (state = initialState, action) => {
+const rocketsReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_ROCKETS:
       return [...state, action.payload];
