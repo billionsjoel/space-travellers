a210e9f768e9d6858da976646edc4330cb907f88
Billions Joel <joelatugonza@gmail.com>
2022-03-01 09:41:29 +0300

    Create no match page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Rocket from './components/pages/rocket';
 import Mission from './components/pages/mission';
 import Profile from './components/pages/profile';
+import Nomatch from './components/pages/nomatch';
 import Header from './components/Header';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Rocket />} />
             <Route path="missions" element={<Mission />} />
             <Route path="my-profile" element={<Profile />} />
+            <Route path="*" element={<Nomatch />} />
           </Routes>
         </main>
       </Router>
