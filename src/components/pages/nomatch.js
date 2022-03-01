9eeb9fc04965bec19bd5a9241ed3636ee68c56bb
Billions Joel <joelatugonza@gmail.com>
2022-03-01 09:41:38 +0300

    Create no match page component

--- src/components/pages/nomatch.js
+++ src/components/pages/nomatch.js
@@ -1,3 +1,15 @@
 import { NavLink } from 'react-router-dom';
 
+function nomatch() {
+  return (
+    <div>
+      <h3>
+        404 . Page does not exist. Go back to
+        {' '}
+        <NavLink to="/">home page.</NavLink>
+      </h3>
+    </div>
+  );
+}
 
+export default nomatch;
