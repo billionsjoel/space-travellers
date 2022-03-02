8133be3cf1bef085dd6063d5c0307041866cc724
Billions Joel <joelatugonza@gmail.com>
2022-03-02 11:08:00 +0300

    Import loadRockets method

--- src/components/pages/rockets/rocket.js
+++ src/components/pages/rockets/rocket.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { loadRockets } from '../../../redux/rockets/rockets';
 import RocketItem from './rocketItem';
 
 const rocket = () => {
