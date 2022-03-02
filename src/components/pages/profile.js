c468005b141c69cbc89e633d23c7b820008286a9
Billions Joel <joelatugonza@gmail.com>
2022-03-02 15:57:27 +0300

    Refactor profile style

--- src/components/pages/profile.js
+++ src/components/pages/profile.js
@@ -10,29 +10,29 @@ const profile = () => {
   const reservedRockets = rockets.filter((rockets) => rockets.reserved);
 
   return (
-    <div className="profile-container d-flex jc-c">
-      <div className="profile-missions">
-        <p className="bold tableTitle">My Missions</p>
-        <table>
-          {joinedMissions.map((mission) => (
-            <tr key={mission.id}>
-              <td>{mission.name}</td>
-            </tr>
-          ))}
-        </table>
-      </div>
-      <div className="profile-rockets">
-        <p className="bold tableTitle">My Rockets</p>
-        <div className="rockets-profile">
-          {reservedRockets.map((rocket) => (
-            <ul key={rocket.id}>
-              <li>{rocket.company}</li>
-            </ul>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+		<div className="profile-container d-flex jc-c">
+			<div className="profile-missions">
+				<p className="bold tableTitle">My Missions</p>
+				<div className="profile-list">
+					{joinedMissions.map((mission) => (
+						<ul key={mission.id}>
+							<li>{mission.name}</li>
+						</ul>
+					))}
+				</div>
+			</div>
+			<div className="profile-rockets">
+				<p className="bold tableTitle">My Rockets</p>
+				<div className="profile-list">
+					{reservedRockets.map((rocket) => (
+						<ul key={rocket.id}>
+							<li>{rocket.company}</li>
+						</ul>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default profile;
