7d83edb572f48e6a7eedf89ab66adf54b4ff4ce6
Billions Joel <joelatugonza@gmail.com>
2022-03-02 10:59:21 +0300

    Define ADD_ROCKET action constant

--- src/components/pages/rockets/rocketItem.js
+++ src/components/pages/rockets/rocketItem.js
@@ -1,16 +1,34 @@
-function rocketItem({ company, description, image }) {
-  return (
-    <div className="rocket-container">
-      <div className="image">
-        <img src={image} alt="" />
-      </div>
-      <div className="rocket-details">
-        <h4>{company}</h4>
-        <p>{description}</p>
-        <button type="button">Reserve</button>
-      </div>
+// import PropTypes from 'prop-types';
+
+const rocketItem = (props) => {
+  const { rocketsList } = props;
+  return rocketsList.map((rocket) => (
+    <div className="rocket-container" key={rocket.id}>
+      <>
+        <div className="image">
+          <img src={rocket.image} alt="" />
+        </div>
+        <div className="rocket-details">
+          <h4>{rocket.company}</h4>
+          <p>{rocket.description}</p>
+          <button type="button" value={rocket.reserved}>
+            Reserve
+          </button>
+        </div>
+      </>
     </div>
-  );
-}
+  ));
+};
+
+// rocketItem.propTypes = {
+//  rocketsList: PropTypes.arrayOf(
+//    PropTypes.shape({
+//      id: PropTypes.string.isRequired,
+//      company: PropTypes.string.isRequired,
+//      description: PropTypes.string.isRequired,
+//      joined: PropTypes.bool.isRequired,
+//    }),
+//  ).isRequired,
+// };
 
 export default rocketItem;
