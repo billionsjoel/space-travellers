d51949d2445a6b07d8b3bdd7921ab0f4955b21e5
Billions Joel <joelatugonza@gmail.com>
2022-03-02 11:06:40 +0300

    Use effect to dispatch loadRockets method

--- src/components/pages/rockets/rocket.js
+++ src/components/pages/rockets/rocket.js
@@ -6,7 +6,10 @@ import RocketItem from './rocketItem';
 const rocket = () => {
   const rocketsList = useSelector(({ rocketsReducer }) => rocketsReducer);
   const dispatch = useDispatch();
-
+  useEffect(() => {
+    if (!rocketsList.length) {
+      dispatch(loadRockets());
+    }
   }, []);
 
   return (
